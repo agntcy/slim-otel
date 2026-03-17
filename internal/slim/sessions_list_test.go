65277f65502bd1884f1abb62907b07e23aca9667
Michele Papalini <micpapal@cisco.com>
2026-03-17 16:59:37 +0100

    fix builder config
    Signed-off-by: Michele Papalini <micpapal@cisco.com>

--- internal/slim/sessions_list_test.go
+++ internal/slim/sessions_list_test.go
@@ -7,9 +7,10 @@ import (
 	"sync"
 	"testing"
 
-	slimconfig "github.com/agntcy/slim-otel/slimconfig"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/agntcy/slim-otel/slimconfig"
 )
 
 // TestNewSessionsList tests creating a new SessionsList
